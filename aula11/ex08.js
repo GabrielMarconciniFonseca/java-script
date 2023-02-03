658b7a21a0ba374814c0c47ff7b59b6957fe65dd
Gabriel Fonseca <gabriel.fonseca@ciandt.com>
2023-02-03 16:47:23 -0300

    condições( If else )

---
+++ aula11/ex08.js
@@ -0,0 +1,8 @@
+const vel = 59;
+
+console.log(`Sua velocidade é ${vel} hm/h`);
+if (vel > 60) {
+  console.log("Ultrapassou o limite de velocidade permitido(MULTADO)!");
+} else {
+  console.log(`Diraja sempre usando cinto de segurança`);
+}
